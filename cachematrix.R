38af07c0a4489f408a7d22126d8aa0cf2623cea4
Andrew Stein <ahstein@gmail.com>
2014-12-01 17:08:40 -0800

    Seems to work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse
+## of a matrix rather than computing it repeatedly (there are also alternatives to matrix inversion that we will not discuss here).
+## 
 
-## Write a short comment describing this function
+#  Overview
+#1. makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+#2. cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already
+#been calculated (and the matrix has not changed), then the  cachesolve should retrieve the inverse from the cache.
 
+# taken from example makeVector code:
+
+# set the value of the matrix
+# get the value of the matrix
+# set the inverse of the matrix
+# get the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
+	this_inv <-NULL
+	set <-function(y){
+		x<<- y
+		this_inv<<-NULL
+	}#function 
+	get <- function()x
+	set_inverse <- function(inverse) this_inv <<- inverse
+	get_inverse <- function() this_inv
+	list(set=set,get=get,set_inverse=set_inverse,get_inverse=get_inverse)
+}# makeCacheMatix
 
-}
-
-
-## Write a short comment describing this function
-
+# no error checking for inverse of matrix is possible
+# interaction with the makeCacheMatrix object above.
+# based on the example makeVector code
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+	this_inv<- x$get_inverse()
+	if (!is.null(this_inv)){
+		message("getting CACHED inverse matrix")
+		return(this_inv)
+	}#if
+	data<-x$get() 
+	this_inv <-solve(data,...)
+	x$set_inverse(this_inv)
+	this_inv
+}# cacheSolve
+x<- rbind(c(1, -1/4), c(-1/4, 1))
+class(x)
+m = makeCacheMatrix(x)
+m$get()
+m$get_inverse()
+cacheSolve(x)
+cacheSolve(m)
